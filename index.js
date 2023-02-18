ec7a6c84f3007a7dfb4c630e54964407e4218d2b
k8pai <thek8pai@gmail.com>
2023-02-18 12:40:38 +0530

    added individual delete button

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ showLinks.addEventListener('click', () => {
 })
 
 function generateData(value) {
-	return { id: Math.floor(Math.random()*10000000), url: value }
+	return { id: Math.floor(Math.random()*100000000000), url: value }
 }
 
 
@@ -32,6 +32,11 @@ function renderElement(){
 				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 copy-button" data-url="${el.url}">
 					<path class="copy-button" data-url="${el.url}" stroke-linecap="round" stroke-linejoin="round" d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0118 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3l1.5 1.5 3-3.75" />
 				</svg>
+			</button>
+			<button data-id="${el.id}" class='delete-button p-1 mx-2 rounded-md border-2 border-transparent hover:border-green-400/80'>
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" data-id="${el.id}" class="delete-button w-6 h-6">
+					<path data-id="${el.id}" class="delete-button" stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+				</svg>		  
 			</button>
 		</li>`
 	)).join('');
@@ -77,4 +82,14 @@ urlUlContainer.addEventListener('click', function(event) {
                 console.error('Error copying text to clipboard:', error);
             });
     }
+	if (event.target.classList.contains('delete-button')) {
+        savedUrls = JSON.parse(localStorage.getItem('urlsaver')) || [];
+		const data = savedUrls.filter(el => el.id != event.target.dataset.id)
+		localStorage.setItem('urlsaver', JSON.stringify(data));
+		renderElement()
+    }
+});
+
+urlUlContainer.addEventListener('click', function(event) {
+    
 });
