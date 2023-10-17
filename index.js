6affb7a28905a268a25cee25f2f4f045f7512a0c
k8pai <thek8pai@gmail.com>
2023-10-18 02:53:22 +0530

    Updated UI Modified structure including title and favicon of urls to the db

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 let savedUrls = [];
 const email = document.getElementById('email_field');
 const emailInfo = document.getElementById('email-info');
-const savemail = document.getElementById('saveEmail');
+const savemail = document.getElementById('save_email');
 const savedmails = document.getElementById('savedMails');
 const saveUrl = document.getElementById('saveUrl');
 const deleteAll = document.getElementById('deleteAll');
 const windowTabs = document.getElementById('window');
 const urlUlContainer = document.getElementById('urlUlContainer');
 const copyButtons = document.querySelectorAll('.copy-button');
+const cloudButtons = document.querySelectorAll('.cloud-button');
 
 savemail.addEventListener('click', () => {
 	// add the current mail id to localstorage.
@@ -27,11 +28,20 @@ savemail.addEventListener('click', () => {
 });
 
 function generateDataForArray(tabs) {
-	return tabs.map((el, _) => {
-		let url = el.url;
-		let title = getDomain(el.url);
-		return generateData(url, title);
-	});
+	return tabs.map(({ favIconUrl, url, title }, _) => ({
+		id: Math.floor(Math.random() * 100000000000),
+		url,
+		title,
+		icon: favIconUrl,
+	}));
+}
+
+function generateData(value, title) {
+	return {
+		id: Math.floor(Math.random() * 100000000000),
+		url: value,
+		title: title,
+	};
 }
 
 const getDomain = (link) => {
@@ -49,14 +59,6 @@ windowTabs.addEventListener('click', function (event) {
 	});
 });
 
-function generateData(value, title) {
-	return {
-		id: Math.floor(Math.random() * 100000000000),
-		url: value,
-		title: title,
-	};
-}
-
 function renderElement() {
 	let listItems = '';
 	savedUrls = JSON.parse(localStorage.getItem('urlsaver')) || [];
@@ -67,32 +69,37 @@ function renderElement() {
 	} else {
 		urlUlContainer.classList.add('short');
 	}
-	// console.log('largeOrShort = ', largeOrShort);
+
 	listItems = savedUrls
 		?.map(
-			(el) =>
+			({ id, url, title, icon }, _) =>
 				`<li class="mt-2 flex items-center text-blue-400 bg-transparent font-semibold text-base">
-					<input class="p-1 flex-grow border-l-2 cursor-default focus:outline-none focus:ring-0 border-cyan-500/90" readonly type='text' value='${el.title}' />
-					<a title="Visit Link" class='p-1 rounded-md border-2 border-transparent hover:border-green-400/80' target='_blank' href='${el.url}'>
-						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-							<path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
-						</svg>
-					</a>
-					<button title="Copy" data-url="${el.url}" class='copy-button p-1 ml-1 rounded-md border-2 border-transparent hover:border-green-400/80'>
-						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 copy-button" data-url="${el.url}">
-							<path class="copy-button" data-url="${el.url}" stroke-linecap="round" stroke-linejoin="round" d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0118 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3l1.5 1.5 3-3.75" />
-						</svg>
-					</button>
-					<button title="Delete" data-id="${el.id}" data-url="${el.url}" class='cloud p-1 ml-1 rounded-md border-2 border-transparent hover:border-green-400/80'>
-						<svg data-id="${el.id}" data-url="${el.url}" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 cloud">
-							<path class="cloud" data-id="${el.id}" data-url="${el.url}" stroke-linecap="round" stroke-linejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
-						</svg>
-					</button>
-					<button title="Delete" data-id="${el.id}" class='delete-button p-1 ml-1 rounded-md border-2 border-transparent hover:border-green-400/80'>
-						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" data-id="${el.id}" class="delete-button w-6 h-6">
-							<path data-id="${el.id}" class="delete-button" stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-						</svg>		  
-					</button>
+					<div class="flex-col items-start flex-grow">
+						<div class="text-sm text-zinc-600">
+							<input class="px-1 cursor-default focus:outline-none focus:ring-0" title="${url}" readonly type='text' value='${title}' />
+						</div>
+						<div class="text-xs">
+							<a class="px-1 inline-block" title="${url}" target='_blank' href='${url}'>visit link</a>
+						</div>
+					</div>
+					
+					<div id="actions-${id}" class="flex items-center">
+						<button id="copy-button-${id}" title="Copy" data-url="${url}" class='copy-button ml-1 rounded-md border-2 border-transparent transition-all hover:scale-110 text-zinc-600 hover:text-blue-500'>
+							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 copy-button" data-url="${url}">
+								<path class="copy-button" data-url="${url}" stroke-linecap="round" stroke-linejoin="round" d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0118 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3l1.5 1.5 3-3.75" />
+							</svg>
+						</button>
+						<button id="cloud-button-${id}" title="Save to Database" data-id="${id}" data-url="${url}" data-title="${title}" data-icon="${icon}"class='cloud-button ml-1 rounded-md border-2 border-transparent transition-all hover:scale-110 text-zinc-600 hover:text-blue-500'>
+							<svg data-id="${id}" data-url="${url}" data-title="${title}" data-icon="${icon}" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 cloud-button">
+								<path class="cloud-button" data-id="${id}" data-url="${url}" data-title="${title}" data-icon="${icon}" stroke-linecap="round" stroke-linejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
+							</svg>
+						</button>
+						<button id="delete-button-${id}" title="Delete" data-id="${id}" class='delete-button ml-1 rounded-md border-2 border-transparent transition-all hover:animate-wiggle text-zinc-600 hover:text-red-500'>
+							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" data-id="${id}" class="delete-button w-5 h-5">
+								<path data-id="${id}" class="delete-button" stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+							</svg>		  
+						</button>
+					</div>
 				</li>`,
 		)
 		.join('');
@@ -105,16 +112,15 @@ deleteAll.addEventListener('click', () => {
 });
 
 saveUrl.addEventListener('click', () => {
-	console.log('function invoked');
 	chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
 		var activeTab = tabs[0];
-		// console.log('activeTab = ', activeTab);
+		console.log('activeTab = ', activeTab);
 		savedUrls = JSON.parse(localStorage.getItem('urlsaver')) || [];
 		const title = activeTab.url.replace(
 			/* prettier-ignore */ /^https?:\/\//,
 			'',
 		);
-		// console.log('title = ', title);
+		console.log('title = ', title);
 		savedUrls.unshift(generateData(activeTab.url, title));
 		localStorage.setItem('urlsaver', JSON.stringify(savedUrls));
 
@@ -123,7 +129,7 @@ saveUrl.addEventListener('click', () => {
 	});
 });
 
-urlUlContainer.addEventListener('click', function (event) {
+urlUlContainer.addEventListener('click', async (event) => {
 	if (event.target.classList.contains('copy-button')) {
 		const url = event.target.dataset.url;
 		navigator.clipboard
@@ -142,8 +148,37 @@ urlUlContainer.addEventListener('click', function (event) {
 		renderElement();
 	}
 
-	if (event.target.classList.contains('cloud')) {
-		saveLinkToDb(event.target.dataset.url, email.value);
+	if (event.target.classList.contains('cloud-button')) {
+		const val = ({
+			id: datasetId,
+			url: datasetUrl,
+			title: datasetTitle,
+			icon: datasetIcon,
+		} = event.target.dataset);
+
+		// console.log('val => ', val, val.id, val.icon, val.title, val.url);
+
+		let copybtn = document.getElementById(`copy-button-${datasetId}`);
+		let cloudbtn = document.getElementById(`cloud-button-${datasetId}`);
+		let deletebtn = document.getElementById(`delete-button-${datasetId}`);
+
+		if (!email.value) {
+			email.classList.remove('border-green-400/80');
+			email.classList.add('border-red-400/80');
+			// console.log('from !mail');
+			emailInfo.textContent =
+				'Select one of your saved mails or type in your mail to save link to linkwrap';
+			return;
+		}
+
+		copybtn.disabled = true;
+		cloudbtn.disabled = true;
+		deletebtn.disabled = true;
+
+		let res = await saveLinkToDb(val, email.value);
+		if (!res) {
+			return;
+		}
 		savedUrls = JSON.parse(localStorage.getItem('urlsaver')) || [];
 		const data = savedUrls.filter((el) => el.id != event.target.dataset.id);
 		localStorage.setItem('urlsaver', JSON.stringify(data));
@@ -172,37 +207,9 @@ document.addEventListener('DOMContentLoaded', async function () {
 		)
 		.join('');
 
-	listItems = savedUrls
-		?.map(
-			(el) =>
-				`<li class="mt-2 flex items-center text-blue-400 bg-transparent font-semibold text-base">
-				<input class="p-1 flex-grow border-l-2 cursor-default focus:outline-none focus:ring-0 border-cyan-500/90" readonly type='text' value='${el.title}' />
-				<a title="Visit Link" class='p-1 rounded-md border-2 border-transparent hover:border-green-400/80' target='_blank' href='${el.url}'>
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-						<path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
-					</svg>
-				</a>
-				<button title="Copy" data-url="${el.url}" class='copy-button p-1 ml-1 rounded-md border-2 border-transparent hover:border-green-400/80'>
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 copy-button" data-url="${el.url}">
-						<path class="copy-button" data-url="${el.url}" stroke-linecap="round" stroke-linejoin="round" d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0118 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3l1.5 1.5 3-3.75" />
-					</svg>
-				</button>
-				<button title="Save" data-id="${el.id}" data-url="${el.url}" class='cloud p-1 ml-1 rounded-md border-2 border-transparent hover:border-green-400/80'>
-					<svg data-id="${el.id}" data-url="${el.url}" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 cloud">
-						<path class="cloud" data-id="${el.id}" data-url="${el.url}" stroke-linecap="round" stroke-linejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
-					</svg>
-				</button>
-				<button title="Delete" data-id="${el.id}" class='delete-button p-1 ml-1 rounded-md border-2 border-transparent hover:border-green-400/80'>
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" data-id="${el.id}" class="delete-button w-6 h-6">
-						<path data-id="${el.id}" class="delete-button" stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-					</svg>		  
-				</button>
-			</li>`,
-		)
-		.join('');
+	renderElement();
 
 	savedmails.innerHTML = mailTags;
-	urlUlContainer.innerHTML = listItems;
 
 	const linkList = document.getElementById('linkList');
 
@@ -225,31 +232,15 @@ async function getLinksFromServer() {
 	}
 }
 
-async function saveLinkToDb(link) {
-	const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-	if (!email.value) {
-		email.classList.remove('border-green-400/80');
-		email.classList.add('border-red-400/80');
-		// console.log('from !email.value');
-		emailInfo.textContent =
-			'Select one of your saved mails or type in your mail to save link to linkwrap';
-		return;
-	} else if (!emailPattern.test(email.value)) {
-		email.classList.remove('border-green-400/80');
-		email.classList.add('border-red-400/80');
-		// console.log('from !emailPattern.test(email.value)');
-		emailInfo.textContent =
-			'Select one of your saved mails or type in your mail to save link to linkwrap';
-		return;
-	}
+async function saveLinkToDb({ url, title, icon }, mail) {
 	if (!email.classList.contains('border-green-400/80')) {
 		email.classList.add('border-green-400/80');
 		email.classList.remove('border-red-400/80');
 		emailInfo.textContent = '';
 	}
 
-	const body = { email: email.value, link };
+	const body = { email: mail, link: url, title, icon };
+	console.log('body => ', body);
 	try {
 		const response = await fetch(
 			'https://linkwrap.k8pai.dev/api/extention',
@@ -266,6 +257,6 @@ async function saveLinkToDb(link) {
 		return data;
 	} catch (error) {
 		console.error(error);
-		return [];
+		return undefined;
 	}
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,34 @@
 module.exports = {
 	content: ['./*.{html,js}'],
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+				wiggle: {
+					'0%, 100%': { transform: 'rotate(0deg)' },
+					'25%': { transform: 'rotate(-7deg)' },
+					'75%': { transform: 'rotate(14deg)' },
+					'50%': { transform: 'rotate(7deg)' },
+				},
+				bounce: {
+					'0%, 100%': {
+						transform: 'translateY(0%)',
+						animationTimingFunction: 'cubic-bezier(0.8, 0, 1, 1)',
+					},
+					'25%, 75%': {
+						transform: 'translateY(-15%)',
+						animationTimingFunction: 'cubic-bezier(0.8, 0, 1, 1)',
+					},
+					'50%': {
+						transform: 'translateY(0)',
+						animationTimingFunction: 'cubic-bezier(0, 0, 0.2, 1)',
+					},
+				},
+			},
+			animation: {
+				wiggle: 'wiggle .4s ease-in-out',
+				bounce: 'bounce 2s ease-in 2',
+			},
+		},
 	},
 	plugins: [],
 };
